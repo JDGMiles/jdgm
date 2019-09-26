526b9aefdade9da6872f0a05e9584c10d14e11ed
James Miles <jimdgmiles@gmail.com>
2019-09-26 02:55:02 +0100

    Adjusted measurements.

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
-var siteWidth = 840;
-var siteHeight = 510;
-var scale1 = screen.width /siteWidth;
-var scale2 = screen.height /siteHeight;
+var siteWidth = 531;
+var siteHeight = 853;
+var scale1 = window.innerWidth /siteWidth;
+var scale2 = window.innerHeight /siteHeight;
 var scale = Math.min(scale1, scale2);
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', height='+siteHeight+', initial-scale='+scale+'');
