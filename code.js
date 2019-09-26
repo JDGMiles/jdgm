48ac2d8e5adc2bccea4a8fa00c942c46697e2b4a
James Miles <jimdgmiles@gmail.com>
2019-09-26 01:53:47 +0100

    Updated viewport issues.

---
+++ code.js
@@ -0,0 +1,7 @@
+var siteWidth = 833;
+var siteHeight = 501;
+var scale1 = screen.width /siteWidth;
+var scale2 = screen.height /siteHeight;
+var scale = Math.min(scale1, scale2);
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', height='+siteHeight+', initial-scale='+scale+'');
